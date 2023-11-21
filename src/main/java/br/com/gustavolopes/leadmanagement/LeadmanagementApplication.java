10c9095228104ed05988b75b3ead3b74ecb13e16
Diego-World <contato.diegogouveia@gmail.com>
2023-11-21 14:21:40 -0300

    first commit

---
+++ src/main/java/br/com/gustavolopes/leadmanagement/LeadmanagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.gustavolopes.leadmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gustavolopes/leadmanagement/LeadmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavolopes.leadmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeadmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
